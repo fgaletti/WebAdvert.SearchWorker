6d8391a34c4e434c48b6ac620b3b4e9788b5a146
francisco <franciscogaletti@yahoo.com>
2020-06-27 02:00:10 -0500

    Add project files.

---
+++ SearchWorker.cs
@@ -0,0 +1,31 @@
+﻿using Amazon.Lambda.Core;
+using Amazon.Lambda.SNSEvents;
+using System;
+using Nest;
+
+[assembly:LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
+namespace WebAdvert.SearchWorker
+{
+    //31
+    public class SearchWorker
+    {
+        // pass one instance 
+        public SearchWorker(): this(ElasticSearchHelper.GetInstance(ConfigurationHelper.Instance))
+        {
+
+        }
+
+        private readonly IElasticClient _client;
+        public SearchWorker(IElasticClient client)
+        {
+            _client = client;
+        }
+        public void Function(SNSEvent snsEvent, ILambdaContext context)
+        {
+            foreach (var record in snsEvent.Records)
+            {
+                context.Logger.LogLine(record.Sns.Message);
+            }
+        }
+    }
+}
